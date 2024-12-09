dc346b69b5a4d36790c642dc34f20b9c256fa571
Emre Akguel <emre.akguel.1@gmail.com>
2024-12-09 07:46:24 +0100

    prepare day 09

---
+++ src/main/kotlin/tr/emreone/adventofcode/days/Day09.kt
@@ -0,0 +1,21 @@
+package tr.emreone.adventofcode.days
+
+import tr.emreone.kotlin_utils.Resources
+import tr.emreone.kotlin_utils.automation.Day
+
+class Day09 : Day(
+    9,
+    2024,
+    "",
+    session = Resources.resourceAsString("session.cookie")
+) {
+
+    override fun part1(): Int {
+        return 0
+    }
+
+    override fun part2(): Long {
+        return 0
+    }
+
+}

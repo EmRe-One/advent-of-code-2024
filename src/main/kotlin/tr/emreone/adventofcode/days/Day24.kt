36e85d3d01471c680e1a561f19ff9fdc08d23971
Emre Akgül (CarTelSol) <e.akguel@cartelsol.com>
2024-12-24 13:22:51 +0100

    start solving 24

--- src/main/kotlin/tr/emreone/adventofcode/days/Day24.kt
+++ src/main/kotlin/tr/emreone/adventofcode/days/Day24.kt
@@ -2,6 +2,7 @@ package tr.emreone.adventofcode.days
 
 import tr.emreone.kotlin_utils.Resources
 import tr.emreone.kotlin_utils.automation.Day
+import tr.emreone.kotlin_utils.automation.log
 
 class Day24 : Day(
     24,
@@ -9,65 +10,143 @@ class Day24 : Day(
     "Crossed Wires",
     session = Resources.resourceAsString("session.cookie")
 ) {
+    private val operators = mapOf(
+        "AND" to { x: Int, y: Int -> x and y },
+        "OR" to { x: Int, y: Int -> x or y },
+        "XOR" to { x: Int, y: Int -> x xor y }
+    )
+
+    private val formulas = inputAsGroups[1].associate { line ->
+        val (input1, operation, input2: String, output) = line.replace(" -> ", " ").split(" ")
+        output to Triple(operation, input1, input2)
+    }
+
+    private fun prettyPrint(wire: String, depth: Int = 0): String {
+        if ("([xy])\\d+".toRegex().matches(wire)) {
+            return "  ".repeat(depth) + wire
+        }
+        val (op, x, y) = formulas[wire]!!
+        return buildString {
+            append("  ".repeat(depth) + "$op ($wire)")
+            append("\n")
+            append(prettyPrint(x, depth + 1))
+            append("\n")
+            append(prettyPrint(y, depth + 1))
+        }
+    }
+
+    private fun verifyIntermediateXor(wire: String, num: Int): Boolean {
+        log { "Verifying intermediate XOR $wire, $num" }
+        val (op, x, y) = formulas[wire]!!
+        if (op != "XOR") return false
+        val xName = "x" + num.toString().padStart(2, '0')
+        val yName = "y" + num.toString().padStart(2, '0')
+
+        return listOf(x, y).sorted() == listOf(xName, yName)
+    }
+
+    private fun verifyCarryBit(wire: String, num: Int): Boolean {
+        log { "Verifying carry bit $wire, $num" }
+        val (op, x, y) = formulas[wire]!!
+
+        if (num == 1) {
+            if (op != "AND") return false
+            return listOf(x, y).sorted() == listOf("x00", "y00")
+        }
+        if (op != "OR") return false
+        return verifyDirectCarry(x, num - 1) && verifyRecarry(y, num - 1)
+                || verifyDirectCarry(y, num - 1) && verifyRecarry(x, num - 1)
+    }
+
+    private fun verifyDirectCarry(wire: String, num: Int): Boolean {
+        log { "Verifying direct carry $wire, $num" }
+        val (op, x, y) = formulas[wire]!!
+
+        if (op != "AND") return false
+
+        val xName = "x" + num.toString().padStart(2, '0')
+        val yName = "y" + num.toString().padStart(2, '0')
+        return listOf(x, y).sorted() == listOf(xName, yName)
+    }
+
+    private fun verifyRecarry(wire: String, num: Int): Boolean {
+        log { "Verifying recarry $wire, $num" }
+        val (op, x, y) = formulas[wire]!!
+
+        if (op != "AND") return false
+
+        return verifyIntermediateXor(x, num) && verifyCarryBit(y, num)
+                || verifyIntermediateXor(y, num) && verifyCarryBit(x, num)
+    }
+
+    fun verifyZ(wire: String, num: Int): Boolean {
+        log { "Verifying $wire, $num" }
+
+        val (op, x, y) = formulas[wire]!!
+        if (op != "XOR") return false
+
+        if (num == 0) {
+            return listOf(x, y).sorted() == listOf("x00", "y00")
+        }
+
+        return verifyIntermediateXor(x, num) && verifyCarryBit(y, num)
+                || verifyIntermediateXor(y, num) && verifyCarryBit(x, num)
+    }
 
     override fun part1(): Long {
-        val wires = mutableMapOf<String, Boolean>()
-        val gates = mutableListOf<List<String>>()
-
-        val initialValues = inputAsGroups[0]
-        val gateConnections = inputAsGroups[1]
-
-        initialValues.forEach { line ->
+        val knownWires = inputAsGroups[0].associate { line ->
             val (wire, value) = line.split(": ")
-            wires[wire.trim()] = value.trim() == "1"
+            wire.trim() to value.trim().toInt()
+        }.toMutableMap()
+
+        fun calc(wire: String): Int {
+            if (wire in knownWires) return knownWires[wire]!!
+
+            val (op, x, y) = formulas[wire]!!
+
+            knownWires[wire] = operators[op]!!(calc(x), calc(y))
+            return knownWires[wire]!!
         }
 
-        gateConnections.forEach { line ->
-            val (input1, operation, input2: String, _: String, output) = line.split(" ")
-            gates.add(listOf(input1, input2, operation, output))
-        }
-
-        // Simulate the gates
-        fun evaluateGate(gate: List<String>): Boolean? {
-            val (input1, input2, operation, output) = gate
-            val value1 = wires[input1]
-            val value2 = wires[input2]
-            if (value1 != null && value2 != null) {
-                return when (operation) {
-                    "AND" -> value1 && value2
-                    "OR" -> value1 || value2
-                    "XOR" -> value1 xor value2
-                    else -> throw IllegalArgumentException("Invalid gate operation: $operation")
-                }
+        // find all Zxx wires
+        val zWires = mutableListOf<Int>()
+        var i = 0
+        while (true) {
+            val wire = buildString {
+                append("z")
+                append(i.toString().padStart(2, '0'))
             }
-            return null
+            println("Calculating $wire")
+            if (wire !in formulas) break
+            zWires.add(calc(wire))
+            i++
         }
 
-        var outputCalculated = false
-        while (!outputCalculated) {
-            outputCalculated = true
-            for (gate in gates) {
-                val outputValue = evaluateGate(gate)
-                if (outputValue != null && !wires.containsKey(gate[3])) {
-                    wires[gate[3]] = outputValue
-                    outputCalculated = false
-                }
-            }
-        }
-
-        // Calculate the decimal output
-        val zWires = wires.filterKeys { it.startsWith("z") }.toSortedMap()
-        val binaryOutput = zWires.values.joinToString("") { if (it) "1" else "0" }
+        val binaryOutput = zWires.reversed().joinToString("")
         val decimalOutput = binaryOutput.toLong(2)
 
-        println("Wires: $wires")
-        println("Binary output: $binaryOutput")
-        println("Decimal output: $decimalOutput")
+        log {
+            "Binary output: $binaryOutput \n" +
+            "Decimal output: $decimalOutput"
+        }
 
         return decimalOutput
     }
 
     override fun part2(): Long {
+        var i = 0
+        while (true) {
+            val name = buildString {
+                append("z")
+                append(i.toString().padStart(2, '0'))
+            }
+            if (!verifyZ(name, i)) {
+                log { "Found the first invalid wire: $name" }
+                break
+            }
+            i++
+        }
+
         return 0
     }
 

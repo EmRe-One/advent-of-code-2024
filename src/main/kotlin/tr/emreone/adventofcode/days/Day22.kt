141022c593024a5e3a080593c11573c4c5263f52
Emre Akguel <emre.akguel.1@gmail.com>
2024-12-24 03:16:31 +0100

    finish day 22

--- src/main/kotlin/tr/emreone/adventofcode/days/Day22.kt
+++ src/main/kotlin/tr/emreone/adventofcode/days/Day22.kt
@@ -2,6 +2,7 @@ package tr.emreone.adventofcode.days
 
 import tr.emreone.kotlin_utils.Resources
 import tr.emreone.kotlin_utils.automation.Day
+import tr.emreone.kotlin_utils.automation.log
 
 class Day22 : Day(
     22,
@@ -10,12 +11,59 @@ class Day22 : Day(
     session = Resources.resourceAsString("session.cookie")
 ) {
 
-    override fun part1(): Int {
-        return 0
+    class SecretNumber {
+        companion object {
+            fun evolveSecretNumber(secret: Long): Long {
+                var randomized = secret
+                randomized = ((randomized * 64) xor randomized) % 16_777_216
+                randomized = ((randomized / 32) xor randomized) % 16_777_216
+                randomized = ((randomized * 2048) xor randomized) % 16_777_216
+
+                return randomized
+            }
+        }
+    }
+
+    override fun part1(): Long {
+        return inputAsList.sumOf { initial ->
+            var num = initial.toLong()
+            repeat(2000) {
+                num = SecretNumber.evolveSecretNumber(num)
+            }
+            num.toLong()
+        }
     }
 
     override fun part2(): Long {
-        return 0
+        val sequenceToTotal = mutableMapOf<List<Long>, Long>()
+
+        inputAsList.forEach { initial ->
+            var num = initial.toLong()
+            val buyer = mutableListOf<Long>()
+            buyer.add(num % 10)
+            repeat(2000) {
+                num = SecretNumber.evolveSecretNumber(num)
+                buyer.add(num % 10)
+            }
+
+            // if the sequence is already seen, skip, because monkey would sell at first opportunity
+            val seen = mutableSetOf<List<Long>>()
+            buyer.windowed(5).forEach { window ->
+                val seq = window.windowed(2).map { it[1] - it[0] }
+                if (seen.contains(seq)) {
+                    return@forEach
+                }
+
+                seen.add(seq)
+                sequenceToTotal[seq] = sequenceToTotal.getOrDefault(seq, 0L) + window.last()
+            }
+        }
+
+        val bestSequence = sequenceToTotal.maxBy { it.value }
+        log {
+            "Best sequence: $bestSequence"
+        }
+        return bestSequence.value
     }
 
 }
--- src/test/kotlin/tr/emreone/adventofcode/days/Day22Test.kt
+++ src/test/kotlin/tr/emreone/adventofcode/days/Day22Test.kt
@@ -7,7 +7,7 @@ fun main() {
 
     solve<Day22>(false) {
         Resources.resourceAsList("day22_example.txt")
-            .joinToString("\n") part1 37_327_623 part2 0
+            .joinToString("\n") part1 37_327_623 part2 24L
     }
 
 }
